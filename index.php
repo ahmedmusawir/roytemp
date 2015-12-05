89623225bde8c3798f083b871e5a6d53b26f9831
Da Moose <ahmed.musawir@hotmail.com>
2015-12-05 14:53:26 +0800

    1st Commit: Bootsass setup and frontpage on going ...

---
+++ index.php
@@ -0,0 +1,225 @@
+<?php include "header.php" ?>
+
+<article class="front-page-slider">
+	
+	<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+	  <ol class="carousel-indicators">
+	    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+	    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+	    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+	  </ol>
+
+	  <!-- Wrapper for slides -->
+	  <div class="carousel-inner" role="listbox">
+	    <div class="item active">
+	      <img src="img/slide1.jpg" alt="...">
+	      <!-- <img src="http://lorempixel.com/1920/400/abstract/1" alt="..."> -->
+	      <div class="carousel-caption">
+	        
+	      </div>
+	    </div>
+	    <div class="item">
+	      <img src="img/slide2.jpg" alt="...">
+	      <!-- <img src="http://lorempixel.com/1920/400/abstract/2" alt="..."> -->
+	      <div class="carousel-caption">
+	        
+	      </div>
+	    </div>
+
+	    <div class="item">
+	      <img src="img/slide3.jpg" alt="...">
+	      <!-- <img src="http://lorempixel.com/1920/400/abstract/3" alt="..."> -->
+	      <div class="carousel-caption">
+	        
+	      </div>
+	    </div>
+	    
+	  </div>
+
+	  <!-- Controls -->
+	  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+	    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+	    <span class="sr-only">Previous</span>
+	  </a>
+	  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+	    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+	    <span class="sr-only">Next</span>
+	  </a>
+	</div>
+
+</article>
+
+<section class="fp-blk-1 container">
+	
+	<div class="row">
+
+		<h2 class="header-txt">FINEST ARCHITECTURAL SELECTION</h2>
+		<em><p>Aboluetely Stunning Design & Functionality</p></em>
+
+
+		<article class="img-box col-md-4">
+
+			<img src="img/sec1_imgbox1.jpg" alt="">
+
+			<h4>WE DEVELOP BRANDS</h4>
+			
+			<p class="img-box-text">
+				We have been creating homes for families in Singapore for over 35 years, first earning an outstanding track record and award-winning expertise in construction.
+				That's why we are listed on the Mainboard of the Singapore Exchange
+			</p>
+
+		</article>
+
+		<article class="img-box col-md-4">
+
+			<img src="img/sec2_imgbox2.jpg" alt="">
+
+			<h4>CUSTOM ESTATE SOLUTIONS</h4>
+			
+			<p class="img-box-text">
+				We are recognised as an established property development and construction company with a broad portfolio of residential, commercial, industrial and retail developments, with core foundations of prime location, quality workmanship efficiency.
+			</p>
+
+		</article>
+
+		<article class="img-box col-md-4">
+
+			<img src="img/sec3_imgbox3.jpg" alt="">
+
+			<h4>REALIZE YOUR DREAMS</h4>
+			
+			<p class="img-box-text">
+				Over the years, we have built a name in the market for quality projects with its creed of maintaining high and consistent standards of quality on every project. This is evident from the various awards and recognition obtained from industry bodies over the years.
+			</p>
+
+		</article>
+
+	</div>
+
+</section>
+
+<style type="text/css" media="screen">
+
+.fp-blk-2 {
+	background: url(img/parallax_01-1024x533.jpg) no-repeat center center fixed; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+	
+</style>
+<section class="fp-blk-2">
+
+	<div class="container">
+
+		<h2 class="header-txt">WHY WANDERVALE?</h2>
+		<em><p>Don't let an opportunity of a life time pass you by ...</p></em>
+
+
+		<article class="img-box col-md-4">
+
+			<i class="fa fa-camera-retro fa-5x"></i> 
+
+			<h4>TRANSPORT</h4>
+			
+			<ul class="img-box-text">
+
+				<li>Choa Chu Kang (NS4/BP1) MRT, LRT and Bus Interchange
+				<li>Bukit Panjang Integrated Transport Hub (LRT/ DTL2/ Bus)
+				<li>Near KJE, connected to PIE and BKE
+			
+			</ul>
+
+		</article>
+
+		<article class="img-box col-md-4">
+
+			<i class="fa fa-camera-retro fa-5x"></i> 
+
+			<h4>BUS</h4>
+			
+			<ul class="img-box-text">
+
+				<li>Choa Chu Kang (NS4/BP1) MRT, LRT and Bus Interchange
+				<li>Bukit Panjang Integrated Transport Hub (LRT/ DTL2/ Bus)
+				<li>Near KJE, connected to PIE and BKE
+			
+			</ul>
+
+		</article>
+
+		<article class="img-box col-md-4">
+
+			<i class="fa fa-camera-retro fa-5x"></i> 
+
+			<h4>SHOPPING MALLS</h4>
+			
+			<ul class="img-box-text">
+
+				<li>Choa Chu Kang (NS4/BP1) MRT, LRT and Bus Interchange
+				<li>Bukit Panjang Integrated Transport Hub (LRT/ DTL2/ Bus)
+				<li>Near KJE, connected to PIE and BKE
+			
+			</ul>
+
+		</article>
+
+			<article class="img-box col-md-4">
+
+			<i class="fa fa-camera-retro fa-5x"></i> 
+
+			<h4>TRANSPORT</h4>
+			
+			<ul class="img-box-text">
+
+				<li>Choa Chu Kang (NS4/BP1) MRT, LRT and Bus Interchange
+				<li>Bukit Panjang Integrated Transport Hub (LRT/ DTL2/ Bus)
+				<li>Near KJE, connected to PIE and BKE
+			
+			</ul>
+
+		</article>
+
+		<article class="img-box col-md-4">
+
+			<i class="fa fa-camera-retro fa-5x"></i> 
+
+			<h4>BUS</h4>
+			
+			<ul class="img-box-text">
+
+				<li>Choa Chu Kang (NS4/BP1) MRT, LRT and Bus Interchange
+				<li>Bukit Panjang Integrated Transport Hub (LRT/ DTL2/ Bus)
+				<li>Near KJE, connected to PIE and BKE
+			
+			</ul>
+
+		</article>
+
+		<article class="img-box col-md-4">
+
+			<i class="fa fa-camera-retro fa-5x"></i> 
+
+			<h4>SHOPPING MALLS</h4>
+			
+			<ul class="img-box-text">
+
+				<li>Choa Chu Kang (NS4/BP1) MRT, LRT and Bus Interchange
+				<li>Bukit Panjang Integrated Transport Hub (LRT/ DTL2/ Bus)
+				<li>Near KJE, connected to PIE and BKE
+			
+			</ul>
+
+		</article>
+
+
+	</div>
+	
+</section>
+
+
+<?php include "footer.php" ?>
+
+
